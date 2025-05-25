ecd0f2374cc58f9a5e1c89b5fc61fb8ac5e2734b
any444v <Radomir Kovalev>
2025-05-25 20:14:49 +0500

    Fix device check

--- build.sh
+++ build.sh
@@ -348,7 +348,7 @@ build_zip() {
     cp build/update-binary build/out/$MODEL/zip/META-INF/com/google/android/update-binary
     cp build/updater-script build/out/$MODEL/zip/META-INF/com/google/android/updater-script
 
-    sed -i "s|#DEVICE_CHECK|getprop(\"ro.product.device\") == \"$MODEL\" || abort(\"Error: this kernel only for $MODEL!\");|" "$UPDATER_SCRIPT"
+    sed -i "s#\#DEVICE_CHECK#getprop(\"ro.product.device\") == \"$MODEL\" || abort(\"Error: this kernel only for $MODEL!\");#" build/out/$MODEL/zip/META-INF/com/google/android/updater-script
 
     version=$(grep -o 'CONFIG_LOCALVERSION="[^"]*"' arch/arm64/configs/$KERNEL_DEFCONFIG | cut -d '"' -f 2)
     version=${version:1}
